b5ee5708660b90779aa5cb523bbd4d91a71f250b
AbdulRazak Abubakari <abdulrazakneate@gmail.com>
2021-06-18 14:20:53 +0000

    init commit

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require ('express');
 
 const app = express();
+//MiddleWares
+app.use('/post',()=>{
+   // console.log('This is middleware running');
+}) 
 
 //ROUTES
  app.get('/',(reg,res)=>{
      res.send('We are on home')
  })
+
+ app.get('/post',(reg,res)=>{
+    res.send('We are on post')
+})
 
 //Start lestening to the server
 
